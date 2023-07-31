85b832f3a811d7c866bb1f837e0a3496fd9d025f
Winston <winsobaggins@yahoo.com.sg>
2023-07-31 18:54:01 +0800

    removes template code

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
 import { Fragment, useEffect, useState } from 'react'
 import { InteractionStatus } from '@azure/msal-browser';
 import { AuthenticatedTemplate, UnauthenticatedTemplate, useIsAuthenticated, useMsal } from '@azure/msal-react';
@@ -131,26 +128,6 @@ function App() {
     </Fragment>
 
   );
-
-  // original template
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
