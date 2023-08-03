8269e1fc2123f4354f3f5aba34bbbc1f1b0fcb7e
Winston <winsobaggins@yahoo.com.sg>
2023-08-03 12:32:24 +0800

    adds bearer decoding

--- azure-functions/FakeUser/index.js
+++ azure-functions/FakeUser/index.js
@@ -1,5 +1,20 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+
+    var bearer = req.headers.authorization.split(' ');
+    bearerToken = bearer[1];
+    var decoded = jwt.decode(bearerToken);
+
+    context.log(decoded);
+
+    if (!!decoded.upn) {
+        decoded.preferred_username = decoded.upn.toLowerCase();
+    } else {
+        decoded.preferred_username = decoded.preferred_username.toLowerCase();
+    }
+
     context.res = {
         body: []
     };
